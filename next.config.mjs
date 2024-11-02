6d00cff22fb15a8677a261041109f790f0378e80
ankonroy <ankonroy31416@gmail.com>
2024-11-02 17:04:41 +0600

    6th attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   output: "export",
   images: {
-    //   formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "image.tmdb.org",
-        // port: "",
-        // pathname: "",
       },
     ],
   },
